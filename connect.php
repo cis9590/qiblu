7233c7698efbfd7810b6434eb96de239ae780271
cis9590 <cis9590class@gmail.com>
2022-11-26 13:40:43 -0500

    UPDATE

--- connect.php
+++ connect.php
@@ -1,4 +1,27 @@
+<?php
+    $yourName = $_POST['yourName'];
+    $number = $_POST['number'];
+    $tableNumber = $_POST['tableNumber'];
+    $date = $_POST['date'];
+    $comment = $_POST['comment'];
+    $fileName = $_POST['fileName'];
 
+    //Database connection 
+    $conn = new mysqli('localhost','root','','qiblu');
+    if($conn->connect_error){
+        die('Connection Failed : '.$conn->connect_error);
+    }else{
+        $stmt - $conn->prepare("insert into registration(yourName, number, tableNumber, date, comment, fileName)
+            values(?,?,?,?,?,?)");
+        $stmt->bind_param("siisss",$yourName,$number,$tableNumber,$date,$comment,$fileName);
+        $stmt->execute():
+        echo "Submitted Successfully...";
+        $stmt->close();
+        $conn->close();
+}
+s
+
+?>
 
       //Let's try this one as a replacement for the above script
 <?php
