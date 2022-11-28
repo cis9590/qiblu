1fbc4f6752d8e3aca66796cd91253522c3b69ce9
cis9590 <112900815+cis9590@users.noreply.github.com>
2022-11-28 17:46:53 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -65,3 +65,21 @@ s
 
 
 ?>
+
+
+//draft2 to test the connection
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
