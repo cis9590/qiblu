d74c350fa09753417122051e05579b47d49ec0be
cis9590 <112900815+cis9590@users.noreply.github.com>
2022-11-29 21:21:04 -0500

    Delete One API directory

--- One API/accelerator (1).py
+++
@@ -1,8 +0,0 @@
-import ipywidgets as widgets
-device = widgets.RadioButtons(
-    options=['GPU Gen9', 'GPU Iris XE Max', 'CPU Xeon 6128', 'CPU Xeon 8153', 'CPU Xeon 8256'],
-    value='GPU Gen9',    
-    description='Device:',
-    disabled=False
-)
-display(device)
